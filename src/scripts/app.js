7491047e12b7c93959b32a22b9d218e0508e63b8
barg-celia <bargoincelia@gmail.com>
2026-05-28 14:09:57 +0200

    update section conséquence page impact

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,23 @@
+const consequences = [
+  "Expérience unique (peut être addictive) → l’expérience est comparable à un saut en parachute car ça permet de prouver le courage, le dépassement de soi, la capacité à gérer le stress.",
+
+  "Deuxième conséquence → ici tu mets ton deuxième texte.",
+
+  "Troisième conséquence → ici tu mets ton troisième texte."
+];
+
+let index = 0;
+
 const btn = document.querySelector(".btn");
 const overlay = document.querySelector(".image-box .overlay");
+const text = document.getElementById("consequence-text");
+
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+function updateText() {
+  text.textContent = consequences[index];
+}
 
 btn.addEventListener("click", () => {
   overlay.classList.toggle("active");
@@ -7,4 +25,24 @@ btn.addEventListener("click", () => {
   btn.textContent = overlay.classList.contains("active")
     ? "Cacher"
     : "Afficher";
+});
+
+nextBtn.addEventListener("click", () => {
+  index++;
+
+  if (index >= consequences.length) {
+    index = 0;
+  }
+
+  updateText();
+});
+
+prevBtn.addEventListener("click", () => {
+  index--;
+
+  if (index < 0) {
+    index = consequences.length - 1;
+  }
+
+  updateText();
 });
