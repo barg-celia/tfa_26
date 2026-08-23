6b90c8b361d4415c0720791401697985a7294c50
barg-celia <145923956+barg-celia@users.noreply.github.com>
2026-08-23 12:39:57 +0200

    ajout burger menu ok version mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -235,3 +235,20 @@ document.querySelectorAll(".node").forEach(node => {
   });
 });
 
+const menuToggle = document.querySelector(".menu__toggle");
+const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu__el a");
+
+if(menuToggle){
+    // click sur le bouton
+    menuToggle.addEventListener("click", menuOpen);
+
+    // click sur les liens 
+    for(let menuLink of menuLinks){
+        menuLink.addEventListener("click", menuOpen);
+    }
+}
+// function pour ouvrir et fermer le menu
+function menuOpen(){
+    menu.classList.toggle("menu--open");
+}
